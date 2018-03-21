b1a105ca51afab1f7d7b41282494bce8dbdb7089
vdegenne <vdegenne@gmail.com>
2018-03-21 13:15:33 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Spring-example-projects
+
+Just few example projects of how Spring Framework works. Useful when you are trying to learn the basics of this framework.
---
+++ actuator/src/main/java/fr/vdegenne/actuatorfinal/ActuatorFinalApplication.java
@@ -0,0 +1,12 @@
+package fr.vdegenne.actuatorfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorFinalApplication.class, args);
+	}
+}
